e4d4398bb9780146a50ff72197d24b6f80192983
SeanieCodes <seanhello12321@gmail.com>
2024-11-08 23:33:44 +0800

    feat: final touches

--- snake.js
+++ snake.js
@@ -1,42 +1,3 @@
-/*
-
-User Stories
-
-Start the game with a button called "Start"
-Snake (a string of shits) appears on the screen and starts moving straight in a random direction
-A shit (food) appears in a random location on the screen
-The snake can be controlled by the player's inputs (4: up, down, left, right)
-5 possible inputs including click
-When the snake eats the food (i.e. comes into contact with the shit), the snake gets longer by 1 shit
-When the snake collides with itself or the sides of the screen, a message saying "Play Again" pops us
-If the snake manages to fill the screen without colliding with itself or the sides, the message "You Win" pops up
-There is a "Play Again" button for both of the above scenarios, which returns the player to the "Start" screen
-
-
-Wireframe
-
-Start screen with game title and Start button
-Game board set upon a colored background with snake and food inside with message and Try Again button after game ends
-
-
-Pseudocode
-
-Game starts up with game title and Start button
-Player clicks Start button and game starts
-Snake (3 shits) appears in the middle of the game board moving in a random direction
-Snake moves in only one direction unless a button is pressed by the player
-If the snake collides with the sides of the board or itself, the game is over
-Food (1 shit) appears randomly on the board where the snake isn't positioned
-Every time the snake collides with the food, its length increases by 1 shit
-A new food appears whenever the snake collides with the food
-When the snake collides with itself or the sides of the screen, a message saying "Play Again" pops us
-If the snake manages to fill the screen without colliding with itself or the sides, the message "You Win" pops up
-Player can click the Play Again button to return to the Start screen after the game is over
-
-
-
-*/
-
 let snake;
 let food;
 let shits = [];
@@ -179,7 +140,6 @@ function updateGame1() {
         gameOver();
         return;
     };
-
     snake.unshift(snakeHead);
     if (snakeHead.x === food.x && snakeHead.y === food.y) {
         genFood();
@@ -211,7 +171,6 @@ function startGame2() {
     if (interval) {
         clearInterval(interval);  
       };
-    
     gameBoard.innerHTML = '';
     gameRunning = true;
     snake = [{ x: 10, y: 10 }];
@@ -257,7 +216,6 @@ function updateGame2() {
         gameOver();
         return;
     }
-
     snake.unshift(snakeHead);
     if (snakeHead.x === food.x && snakeHead.y === food.y) {
         genFood();
